56f5db2cbb0584af04720893c7b3920676915da7
Darcy Beurle <darcy@beurle.id.au>
2017-09-03 20:47:08 +0200

    Removed almost all raw pointers and memory management

--- hybrid.cpp
+++ hybrid.cpp
@@ -31,10 +31,6 @@
 #include <mpi.h>
 #include <omp.h>
 
-using Vector = Eigen::VectorXd;
-using Vector3 = Eigen::Vector3d;
-using SparseMatrix = Eigen::SparseMatrix<double>;
-
 struct Boundary
 {
     std::string name_;
@@ -43,6 +39,15 @@ struct Boundary
     std::vector<int> indices_;
     double value_;
 };
+
+using Vector = Eigen::VectorXd;
+using Vector3 = Eigen::Vector3d;
+using SparseMatrix = Eigen::SparseMatrix<double>;
+
+using Points = std::vector<std::array<double, 3>>;
+using Faces = std::vector<std::array<int, 3>>;
+using Elements = std::vector<std::array<int, 4>>;
+using Boundaries = std::vector<Boundary>;
 
 // Global variables
 constexpr double t_min = 0.00;
@@ -69,47 +74,43 @@ const int N_t = static_cast<int>((t_max - t_min) / Delta_t + 1);
 int bufferSize = 0;
 double* buffer = NULL;
 
-void exchangeData(Vector& T, Boundary* Boundaries, int myN_b)
+void exchangeData(Vector& T, Boundaries& boundaries)
 {
     int yourID = 0;
     int tag = 0;
     MPI_Status status;
 
-    for (int b = 0; b < myN_b; b++)
+    for (int b = 0; b < boundaries.size(); b++)
     {
-        if (Boundaries[b].type_ == "interprocess")
+        if (boundaries[b].type_ == "interprocess")
         {
-            for (int p = 0; p < Boundaries[b].N_; p++)
+            for (int p = 0; p < boundaries[b].N_; p++)
             {
-                buffer[p] = T[Boundaries[b].indices_[p]];
+                buffer[p] = T[boundaries[b].indices_[p]];
             }
-            yourID = static_cast<int>(Boundaries[b].value_);
-            MPI_Bsend(buffer, Boundaries[b].N_, MPI_DOUBLE, yourID, tag, MPI_COMM_WORLD);
+            yourID = static_cast<int>(boundaries[b].value_);
+            MPI_Bsend(buffer, boundaries[b].N_, MPI_DOUBLE, yourID, tag, MPI_COMM_WORLD);
         }
     }
-    for (int b = 0; b < myN_b; b++)
+    for (int b = 0; b < boundaries.size(); b++)
     {
-        if (Boundaries[b].type_ == "interprocess")
+        if (boundaries[b].type_ == "interprocess")
         {
-            yourID = static_cast<int>(Boundaries[b].value_);
-            MPI_Recv(buffer, Boundaries[b].N_, MPI_DOUBLE, yourID, tag, MPI_COMM_WORLD, &status);
-            for (int p = 0; p < Boundaries[b].N_; p++)
+            yourID = static_cast<int>(boundaries[b].value_);
+            MPI_Recv(buffer, boundaries[b].N_, MPI_DOUBLE, yourID, tag, MPI_COMM_WORLD, &status);
+            for (int p = 0; p < boundaries[b].N_; p++)
             {
-                T[Boundaries[b].indices_[p]] += buffer[p];
+                T[boundaries[b].indices_[p]] += buffer[p];
             }
         }
     }
 }
 
 void readData(char* filename,
-              double**& Points,
-              int**& Faces,
-              int**& Elements,
-              Boundary*& Boundaries,
-              int& myN_p,
-              int& myN_f,
-              int& myN_e,
-              int& myN_b,
+              Points& points,
+              Faces& faces,
+              Elements& elements,
+              Boundaries& boundaries,
               std::vector<bool>& yourPoints,
               int myID)
 {
@@ -124,80 +125,66 @@ void readData(char* filename,
 
     int yourID = 0;
 
-    if (myID == 0)
-    {
-        std::cout << "Reading " << filename << "'s... " << std::flush;
-    }
+    if (myID == 0) std::cout << "Reading " << filename << "'s... " << std::flush;
 
     sprintf(myFileName, "%s%d", filename, myID);
     file.open(myFileName);
+
+    int myN_p, myN_f, myN_e, myN_b;
 
     file >> temp >> myN_p;
     file >> temp >> myN_f;
     file >> temp >> myN_e;
     file >> temp >> myN_b;
 
-    Points = new double*[myN_p];
-    Faces = new int*[myN_f];
-    Elements = new int*[myN_e];
-    Boundaries = new Boundary[myN_b];
-    Points[0] = new double[myN_p * numDims];
-    Faces[0] = new int[myN_f * nodesPerFace];
-    Elements[0] = new int[myN_e * nodesPerElement];
-
+    points.resize(myN_p);
+    faces.resize(myN_f);
+    elements.resize(myN_e);
     yourPoints.resize(myN_p, false);
+    boundaries.resize(myN_b);
 
-    for (int p = 1, pp = numDims; p < myN_p; p++, pp += numDims)
+    file >> temp;
+    for (auto& point : points)
     {
-        Points[p] = &Points[0][pp];
-    }
-    for (int f = 1, ff = nodesPerFace; f < myN_f; f++, ff += nodesPerFace)
-    {
-        Faces[f] = &Faces[0][ff];
-    }
-    for (int e = 1, ee = nodesPerElement; e < myN_e; e++, ee += nodesPerElement)
-    {
-        Elements[e] = &Elements[0][ee];
+        file >> point[0] >> point[1] >> point[2];
     }
 
     file >> temp;
-    for (int p = 0; p < myN_p; p++)
+    for (auto& face : faces)
     {
-        file >> Points[p][0] >> Points[p][1] >> Points[p][2];
+        file >> face[0] >> face[1] >> face[2];
     }
 
     file >> temp;
-    for (int f = 0; f < myN_f; f++)
+    for (auto& element : elements)
     {
-        file >> Faces[f][0] >> Faces[f][1] >> Faces[f][2];
-    }
-
-    file >> temp;
-    for (int e = 0; e < myN_e; e++)
-    {
-        file >> Elements[e][0] >> Elements[e][1] >> Elements[e][2] >> Elements[e][3];
+        file >> element[0] >> element[1] >> element[2] >> element[3];
     }
 
     file >> temp;
     for (int b = 0; b < myN_b; b++)
     {
-        file >> Boundaries[b].name_ >> Boundaries[b].type_ >> Boundaries[b].N_;
-        Boundaries[b].indices_.resize(Boundaries[b].N_);
-        for (int n = 0; n < Boundaries[b].N_; n++)
+        file >> boundaries[b].name_ >> boundaries[b].type_ >> boundaries[b].N_;
+
+        boundaries[b].indices_.resize(boundaries[b].N_);
+
+        for (int n = 0; n < boundaries[b].N_; n++)
         {
-            file >> Boundaries[b].indices_[n];
+            file >> boundaries[b].indices_[n];
         }
-        file >> Boundaries[b].value_;
-        if (Boundaries[b].type_ == "interprocess")
+
+        file >> boundaries[b].value_;
+
+        if (boundaries[b].type_ == "interprocess")
         {
             myMaxN_sb++;
-            myMaxN_sp = std::max(myMaxN_sp, Boundaries[b].N_);
-            yourID = static_cast<int>(Boundaries[b].value_);
+            myMaxN_sp = std::max(myMaxN_sp, boundaries[b].N_);
+            yourID = static_cast<int>(boundaries[b].value_);
             if (yourID > myID)
             {
-                for (int p = 0; p < Boundaries[b].N_; p++)
+                for (int p = 0; p < boundaries[b].N_; p++)
                 {
-                    yourPoints[Boundaries[b].indices_[p]] = true;
+                    yourPoints[boundaries[b].indices_[p]] = true;
                 }
             }
         }
@@ -219,9 +206,9 @@ void readData(char* filename,
 void writeData(std::fstream& file,
                double* T,
                int myN_p,
-               double**& Points,
+               Points const& points,
                int myN_e,
-               int**& Elements,
+               Elements const& elements,
                int l,
                int myID)
 {
@@ -247,15 +234,15 @@ void writeData(std::fstream& file,
          << "\n";
     for (int m = 0; m < myN_p; m++)
     {
-        file << Points[m][0] << "\t" << Points[m][1] << "\t" << Points[m][2] << "\n";
+        file << points[m][0] << "\t" << points[m][1] << "\t" << points[m][2] << "\n";
     }
     // Element/Cell Nodes
     file << "CELLS " << myN_e << "\t" << 5 * myN_e << "\n";
     for (int e = 0; e < myN_e; e++)
     {
         file << "4"
-             << "\t" << Elements[e][0] << "\t" << Elements[e][1] << "\t" << Elements[e][2] << "\t"
-             << Elements[e][3] << "\n";
+             << "\t" << elements[e][0] << "\t" << elements[e][1] << "\t" << elements[e][2] << "\t"
+             << elements[e][3] << "\n";
     }
     // Element/Cell types
     file << "CELL_TYPES " << myN_e << "\n";
@@ -284,14 +271,10 @@ void assembleSystem(SparseMatrix& M,
                     SparseMatrix& K,
                     Vector& s,
                     Vector& T,
-                    double** Points,
-                    int** Faces,
-                    int** Elements,
-                    Boundary* Boundaries,
-                    int myN_p,
-                    int myN_f,
-                    int myN_e,
-                    int myN_b,
+                    Points const& points,
+                    Faces const& faces,
+                    Elements const& elements,
+                    Boundaries& boundaries,
                     int myID)
 {
     if (myID == 0) std::cout << "Assembling system... " << std::flush;
@@ -308,19 +291,19 @@ void assembleSystem(SparseMatrix& M,
 
     int Nodes[nodesPerElement] = {0, 0, 0, 0};
 
-    std::vector<double> Omega(myN_e);
-    std::vector<double> Gamma(myN_f);
+    std::vector<double> Omega(elements.size());
+    std::vector<double> Gamma(faces.size());
 
     s.setZero();
 
     // Calculate face areas
-    for (int f = 0; f < myN_f; f++)
+    for (int f = 0; f < faces.size(); f++)
     {
         for (int p = 0; p < nodesPerFace; p++)
         {
-            x[p] = Points[Faces[f][p]][0];
-            y[p] = Points[Faces[f][p]][1];
-            z[p] = Points[Faces[f][p]][2];
+            x[p] = points[faces[f][p]][0];
+            y[p] = points[faces[f][p]][1];
+            z[p] = points[faces[f][p]][2];
         }
         Gamma[f] = std::sqrt(
                        std::pow((y[1] - y[0]) * (z[2] - z[0]) - (z[1] - z[0]) * (y[2] - y[0]), 2)
@@ -330,13 +313,13 @@ void assembleSystem(SparseMatrix& M,
     }
 
     // Calculate element volumes
-    for (int e = 0; e < myN_e; e++)
+    for (int e = 0; e < elements.size(); e++)
     {
         for (int p = 0; p < nodesPerElement; p++)
         {
-            x[p] = Points[Elements[e][p]][0];
-            y[p] = Points[Elements[e][p]][1];
-            z[p] = Points[Elements[e][p]][2];
+            x[p] = points[elements[e][p]][0];
+            y[p] = points[elements[e][p]][1];
+            z[p] = points[elements[e][p]][2];
         }
 
         Omega[e] = std::abs(
@@ -352,14 +335,14 @@ void assembleSystem(SparseMatrix& M,
     // Assemble M, K and s
     std::vector<Eigen::Triplet<double>> K_triplets, M_triplets;
 
-    for (int e = 0; e < myN_e; e++)
+    for (int e = 0; e < elements.size(); e++)
     {
         for (int p = 0; p < nodesPerElement; p++)
         {
-            Nodes[p] = Elements[e][p];
-            x[p] = Points[Nodes[p]][0];
-            y[p] = Points[Nodes[p]][1];
-            z[p] = Points[Nodes[p]][2];
+            Nodes[p] = elements[e][p];
+            x[p] = points[Nodes[p]][0];
+            y[p] = points[Nodes[p]][1];
+            z[p] = points[Nodes[p]][2];
         }
 
         double const G[numDims][nodesPerElement] =
@@ -399,37 +382,37 @@ void assembleSystem(SparseMatrix& M,
     }
 
     // Apply boundary conditions
-    for (int b = 0; b < myN_b; b++)
+    for (int b = 0; b < boundaries.size(); b++)
     {
-        if (Boundaries[b].type_ == "neumann")
+        if (boundaries[b].type_ == "neumann")
         {
-            for (int f = 0; f < Boundaries[b].N_; f++)
+            for (int f = 0; f < boundaries[b].N_; f++)
             {
                 for (int p = 0; p < nodesPerFace; p++)
                 {
-                    Nodes[p] = Faces[Boundaries[b].indices_[f]][p];
+                    Nodes[p] = faces[boundaries[b].indices_[f]][p];
                     auto const m = Nodes[p];
-                    s[m] += neumann_source_constant * Gamma[Boundaries[b].indices_[f]];
+                    s[m] += neumann_source_constant * Gamma[boundaries[b].indices_[f]];
                 }
             }
         }
-        else if (Boundaries[b].type_ == "robin")
+        else if (boundaries[b].type_ == "robin")
         {
-            for (int f = 0; f < Boundaries[b].N_; f++)
+            for (int f = 0; f < boundaries[b].N_; f++)
             {
                 for (int p = 0; p < nodesPerFace; p++)
                 {
-                    Nodes[p] = Faces[Boundaries[b].indices_[f]][p];
+                    Nodes[p] = faces[boundaries[b].indices_[f]][p];
                     auto const m = Nodes[p];
-                    s[m] -= robin_source_constant * Gamma[Boundaries[b].indices_[f]];
+                    s[m] -= robin_source_constant * Gamma[boundaries[b].indices_[f]];
 
                     for (int q = 0; q < nodesPerFace; q++)
                     {
-                        Nodes[q] = Faces[Boundaries[b].indices_[f]][q];
+                        Nodes[q] = faces[boundaries[b].indices_[f]][q];
                         auto const n = Nodes[q];
                         K_triplets.emplace_back(m,
                                                 n,
-                                                Gamma[Boundaries[b].indices_[f]]
+                                                Gamma[boundaries[b].indices_[f]]
                                                     * robin_stiffness_constant * K_e[p][q]);
                     }
                 }
@@ -437,8 +420,8 @@ void assembleSystem(SparseMatrix& M,
         }
     }
 
-    M.resize(myN_p, myN_p);
-    K.resize(myN_p, myN_p);
+    M.resize(points.size(), points.size());
+    K.resize(points.size(), points.size());
 
     K.setFromTriplets(K_triplets.begin(), K_triplets.end());
     M.setFromTriplets(M_triplets.begin(), M_triplets.end());
@@ -470,9 +453,8 @@ double computeInnerProduct(Vector const& v1,
 void linear_solve(SparseMatrix& A,
                   Vector& u,
                   Vector& b,
-                  Boundary* Boundaries,
+                  Boundaries& boundaries,
                   std::vector<bool> const& yourPoints,
-                  int myN_b,
                   int myID)
 {
     auto const N_row = A.rows();
@@ -482,7 +464,7 @@ void linear_solve(SparseMatrix& A,
     // Compute the initial residual
     Vector Au = A * u;
 
-    exchangeData(Au, Boundaries, myN_b);
+    exchangeData(Au, boundaries);
 
     Vector r_old = b - Au;
 
@@ -500,7 +482,7 @@ void linear_solve(SparseMatrix& A,
     {
         Vector Ad = A * d;
 
-        exchangeData(Ad, Boundaries, myN_b);
+        exchangeData(Ad, boundaries);
 
         auto const dTAd = computeInnerProduct(d, Ad, yourPoints, N_row);
 
@@ -530,23 +512,16 @@ void linear_solve(SparseMatrix& A,
 int main(int argc, char** argv)
 {
     // Memory Allocation
-    double** Points = NULL;
-    int** Faces = NULL;
-    int** Elements = NULL;
-    Boundary* Boundaries = NULL;
-
+    Points points;
+    Faces faces;
+    Elements elements;
+    Boundaries boundaries;
     std::vector<bool> yourPoints;
 
     double* buffer = NULL;
-    int myN_p = 0;
-    int myN_f = 0;
-    int myN_e = 0;
-    int myN_b = 0;
 
     int myID = 0;
     int N_Processes = 0;
-
-    double t = 0.0;
 
     std::fstream file;
 
@@ -563,25 +538,25 @@ int main(int argc, char** argv)
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    readData(argv[1], Points, Faces, Elements, Boundaries, myN_p, myN_f, myN_e, myN_b, yourPoints, myID);
+    readData(argv[1], points, faces, elements, boundaries, yourPoints, myID);
 
     double wtime = (myID == 0) ? MPI_Wtime() : 0.0;
 
     // Set initial condition
-    t = t_min;
-    Vector u = Vector::Ones(myN_p) * T_air;
+    double t = t_min;
+    Vector u = Vector::Ones(points.size()) * T_air;
 
     // Allocate arrays
-    Vector s(myN_p), b(myN_p);
+    Vector s(points.size()), b(points.size());
     SparseMatrix M, K;
 
-    assembleSystem(M, K, s, u, Points, Faces, Elements, Boundaries, myN_p, myN_f, myN_e, myN_b, myID);
+    assembleSystem(M, K, s, u, points, faces, elements, boundaries, myID);
 
     SparseMatrix A = M - Delta_t * K;
 
-    exchangeData(s, Boundaries, myN_b);
+    exchangeData(s, boundaries);
 
-    // writeData(file, T, myN_p, Points, myN_e, Elements,0,myID);
+    // writeData(file, T, myN_p, points, myN_e, elements,0,myID);
 
     // Get Max Temperature
     tempTmax = u.maxCoeff();
@@ -596,11 +571,11 @@ int main(int argc, char** argv)
 
         b = M * u;
 
-        exchangeData(b, Boundaries, myN_b);
+        exchangeData(b, boundaries);
 
         b += Delta_t * s;
 
-        linear_solve(A, u, b, Boundaries, yourPoints, myN_b, myID);
+        linear_solve(A, u, b, boundaries, yourPoints, myID);
 
         // Get Max Temperature
         tempTmax = u.maxCoeff();
@@ -623,15 +598,6 @@ int main(int argc, char** argv)
     }
 
     MPI_Buffer_detach(&buffer, &bufferSize);
-
-    // Deallocate arrays
-    delete[] Points[0];
-    delete[] Points;
-    delete[] Faces[0];
-    delete[] Faces;
-    delete[] Elements[0];
-    delete[] Elements;
-    delete[] Boundaries;
 
     delete[] buffer;
 
